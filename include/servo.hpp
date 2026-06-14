66a82591bfad02edad3e28b42513ca3293b05725
enzo <enzo170506@gmail.com>
2026-06-14 12:16:33 -0300

    feat: servo.hpp + servo.cpp

--- include/servo.hpp
+++ include/servo.hpp
@@ -1,20 +1,41 @@
 #ifdef SERVO_HPP
 #define SERVO_HPP
 
-#include <functional> //Implementa os tipos function<tipo> (ponteiros pra funções genéricas do tipo especificado)
-
 struct ServoConfig
 {
     int pin;
-    int min;
-    int max;
+    int minPulse;
+    int maxPulse;
     int minAngle;
     int maxAngle;
 };
 
 class Servo
 {
-    public:
-        explicit Servo(const ServoConfig& config); //Construtor. O & 
+public:
+    explicit Servo(const ServoConfig& config); //Construtor. O & faz o compilador referenciar config e não copiar seus valores
 
-}
+    void moveToAngle(double angle);
+    void moveByDegrees(double angle);
+    void moveByPercent(double percent);
+
+    void moveToCenter();
+
+    void sleep();
+    void wake();
+
+    double getCurrentAngle(const);
+
+private:
+    void updateServo();
+    int calculatePulseWidth(double angle) const; //Para usar o PWM
+
+private:
+    ServoConfig conf_;
+
+    double currentAngle_;
+    int lastPulseWidth_;
+    bool sleeping_;
+};
+
+#endif
---
+++ src/mime_port/Servo.cpp
@@ -0,0 +1,86 @@
+#include <pigpio.h>
+#include <cmath>
+#include "include/servo.hpp"
+
+//Declarando o contructor
+Servo::Servo(const Servoconfig& config)
+    //Valores de inicialização
+    conf_ = config;
+    currentAngle_ = 0,
+    lastPulseWidth_ = 0,
+    sleeping_ = false;
+
+    //Corpo do constructor
+    {
+        gpioSetMode(conf_.pin, PI_OUTPUT);
+    }
+
+//Declaração de funções
+Servo::moveToAngle(double angle)
+{
+    if (angle < conf_.minAngle)
+    {
+        currentAngle_ = conf_.minAngle;
+    }
+
+    else if (angle < conf_.maxAngle)
+    {
+        currentAngle_ = conf_.maxAngle;
+    }
+
+    else
+    {
+        currentAngle_ = angle;
+    }
+
+    updateServo(); //Repasse o movimento pro servo
+}
+
+void Servo::moveByDegrees(double angle)
+{
+    moveToAngle(currentAngle_ + angle);
+}
+
+void Servo::moveByPercent(double percent) //moveByPercent(50) deixa o servo 50% aberto
+{   
+    double delta = ((conf_.maxAngle - conf_.minAngle)/100.0) * percent;
+    moveToAngle(currentAngle_ + delta);
+}
+
+void Servo::moveToCenter()
+{
+    double center = (conf_.maxAngle + conf_.minAngle)/2.0;
+    moveToAngle(center);
+}
+
+void Servo::sleep() //Sleep Assíncrono
+{
+    gpioWrite(conf_.pin, 0);
+    sleeping_ = true;
+}
+
+void Servo::wake()
+{
+    if (sleeping_)
+    {
+        updateServo();
+        sleeping_ = false;
+    }
+}
+
+void Servo::updateServo() //Atualiza a posição do servo
+{
+    int PulseWidth = calculatePulseWidth(currentAngle_);
+    lastPulseWidth_ = PulseWidth;
+
+    gpioWrite(conf_.pin, PulseWidth);
+}
+
+void Servo::calculatePulseWidth(double angle) const //const garante que a função não altere atributos da classe
+{
+    return static_cast<int>(
+        std::floor(
+            conf_.maxPulse - 
+        )
+    )
+}
